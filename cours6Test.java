24b208d3b404c6391967e5c69872b91da5bebfad
Alex D'Andrea <dana16@uqo.ca>
2023-11-01 10:10:21 -0400

    fin cours 6

--- cours6Test.java
+++ cours6Test.java
@@ -7,12 +7,12 @@ class cours6Test {
 	@Test
 	void test() {
 		assert (cours6.carre(-2)==4);
+		assert (cours6.carre(0)==0);
+		assert (cours6.carre(9)==81);
 	}
 	void test2() {
-		assert (cours6.carre(0)==0);
-	}
-	void test3() {
-		assert (cours6.carre(9)==90);
+		assert(cours6.carre(99)==10);
+		
 	}
 
 
