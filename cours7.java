4f9522bdc3931f09466c8a5ba3850d6d8e0fac8d
Alex D'Andrea <dana16@uqo.ca>
2023-11-02 16:16:44 -0400

    cours 7

---
+++ cours7.java
@@ -0,0 +1,18 @@
+public class cours7 {
+    public static void main(String[] args) {
+
+	}
+
+
+    public static int nombreDeCartes (int n){
+        if (n==1){
+            return 2;
+        }
+        else{
+            int cartes = 3*(n-1)+nombreDeCartes(n-1);
+            return cartes;
+        }
+    
+    }
+
+}
