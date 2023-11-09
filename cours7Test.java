a2e96d9620ac68e1862e638acb949a80042c436f
Dandalex <dana16@uqo.ca>
2023-11-09 11:36:13 -0500

    fixed names

--- cours7Test.java
+++ cours7Test.java
@@ -3,7 +3,6 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 
-import devoir1.NombreRemarquables;
 
 class cours7Test {
 
