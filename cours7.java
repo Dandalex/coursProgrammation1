70576183506a144d039f2761fc9561d377d07829
Alex D'Andrea <dana16@uqo.ca>
2023-11-09 14:14:03 -0500

    cours 7

--- cours7.java
+++ cours7.java
@@ -1,8 +1,61 @@
 public class cours7 {
     public static void main(String[] args) {
+      int [] notes = {45,67,89,4,76,34,67,87,53,56,87,54,34,65,34,6,7,4,2,};
 
+      int compteur=0;
+      for (int i =0; i<notes.length; i++){
+            System.out.println(notes[i]);
+            if (notes[i]>60){
+                compteur = compteur+1;
+            }
+      }
+      System.out.println("le nombre de gens avec plus de 60% est "+compteur);
+      double moyenne=0;
+      int somme=0;
+      for(int i=0; i<notes.length; i++){
+        somme = notes[i] + somme;
+        moyenne = somme*1.0/ notes.length;
+      }
+    System.out.println("La moyenne est  "+moyenne); 
+    int NoteLaPLusBasse = noteLaPlusBasse(notes);
+    System.out.println("la note la plus basse est " +NoteLaPLusBasse);
+        
+    
 	}
 
+    public static boolean equalsArray (int [] a,int []b){
+        if (a.length!=b.length){
+            return false;
+        }
+        for (int i =0; i<a.length; i++){
+            if (a[i]!=b[i]){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean ordreCroissant (int[] a){
+        for (int i =1; i<a.length;i++){
+            if(a[i]<a[i-1]){
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+
+    public static int noteLaPlusBasse (int [] notes){
+        int noteLaPLusBasse=notes[0];
+        for (int i=0; i<notes.length; i++){
+            if (notes[i]<noteLaPLusBasse){
+                noteLaPLusBasse = notes[i];
+            }
+        }
+        
+        return noteLaPLusBasse;
+    }
 
     public static int nombreDeCartes (int n){
         if (n<=0) {
--- cours7Test.java
+++ cours7Test.java
@@ -3,8 +3,6 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 
-import devoir1.NombreRemarquables;
-
 class cours7Test {
 
 	@AfterEach
@@ -24,6 +22,31 @@ class cours7Test {
 		int actualValue = cours7.nombreDeCartes(0);
 		assertEquals(expectedValue, actualValue);
 	}
+
+	@Test
+	void testArrayEquals1(){
+		int[] a ={1,2,3,4,5,6,7,8};
+		int[] b ={1,2,3,4,5,6,7,8};
+		boolean expectedValue= true;
+		boolean actualValue = cours7.equalsArray(a,b);
+		assertEquals(expectedValue, actualValue);
+	}
 	
+	@Test
+	void testArrayEquals2(){
+		int[] a ={2,3,4,5,6,7,8,9};
+		int[] b ={1,2,3,4,5,6,7,8};
+		boolean expectedValue= false;
+		boolean actualValue = cours7.equalsArray(a,b);
+		assertEquals(expectedValue, actualValue);
+	}
+	@Test
+	void testOrdreCroissant(){
+		int[] a ={2,3,4,5,6,7,8,9};
+		boolean expectedValue= true;
+		boolean actualValue = cours7.ordreCroissant(a);
+		assertEquals(expectedValue, actualValue);
+	}
+
 
 }
